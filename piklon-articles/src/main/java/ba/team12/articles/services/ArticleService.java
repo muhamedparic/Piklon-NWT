b9db95049bdbb8abc3aa523229ea6f57bcff5e87
muhamedparic <mparic1@etf.unsa.ba>
2018-03-28 00:09:03 +0200

    Popravljeni malo gitignore i dodan file koji ne bi smio faliti

---
+++ piklon-articles/src/main/java/ba/team12/articles/services/ArticleService.java
@@ -0,0 +1,56 @@
+package ba.team12.articles.services;
+
+import ba.team12.articles.models.Article;
+import ba.team12.articles.models.Category;
+import ba.team12.articles.models.Location;
+import ba.team12.articles.repositories.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ArticleService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private LocationService locationService;
+
+    public Article findById(Long id) {
+        Optional<Article> article = articleRepository.findById(id);
+
+        if (article.isPresent())
+            return article.get();
+        else
+            return null;
+    }
+
+    public List<Article> search(String query) {
+        return articleRepository.findByNameContaining(query);
+    }
+
+    public List<Article> findByCategory(String category) {
+        List<Category> categories = categoryService.search(category);
+
+        if (categories.size() == 1)
+            return articleRepository.findByCategory(categories.get(0));
+        else
+            return null;
+    }
+
+    public List<Article> findByCategoryAndLocation(String category, String locationName) {
+        List<Category> categories = categoryService.search(category);
+        Location location = locationService.findByExactName(locationName);
+
+        if (categories.size() == 1 && location != null)
+            return articleRepository.findByCategoryAndLocation(categories.get(0), location);
+        else
+            return null;
+    }
+}
