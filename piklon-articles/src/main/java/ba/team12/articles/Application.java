7e9d21d6c0e10c335801cdadffa2e75690433aed
muhamedparic <mparic1@etf.unsa.ba>
2018-04-02 23:36:24 +0200

    Eureka server i client

--- piklon-articles/src/main/java/ba/team12/articles/Application.java
+++ piklon-articles/src/main/java/ba/team12/articles/Application.java
@@ -2,10 +2,12 @@ package ba.team12.articles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
