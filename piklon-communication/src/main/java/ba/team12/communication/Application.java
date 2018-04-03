baa75f5fd33feeefd4f470d7e233ebdcbe3e4172
anjakin <anja261295@gmail.com>
2018-04-03 20:58:48 +0200

    fuck my fucking life

--- piklon-communication/src/main/java/ba/team12/communication/Application.java
+++ piklon-communication/src/main/java/ba/team12/communication/Application.java
@@ -2,10 +2,12 @@ package ba.team12.communication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("ba.team12.communication.repositories")
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
