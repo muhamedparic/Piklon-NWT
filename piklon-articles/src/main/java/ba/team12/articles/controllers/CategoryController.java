5a000653b55dfe66703c799ba3c67d887fce6d7c
alen1996 <alen.trueteller@hotmail.com>
2018-04-10 19:51:23 +0200

    sinhrona komunikacija

--- piklon-articles/src/main/java/ba/team12/articles/controllers/CategoryController.java
+++ piklon-articles/src/main/java/ba/team12/articles/controllers/CategoryController.java
@@ -4,9 +4,17 @@ import ba.team12.articles.models.Location;
 import ba.team12.articles.services.CategoryService;
 import ba.team12.articles.services.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -16,6 +24,19 @@ public class CategoryController {
     private CategoryService categoryService;
     @GetMapping("/categories/all")
     public ResponseEntity findAll() {
+        final String uri = "http://localhost:8080/locations/all";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        ResponseEntity<String> response=null;
+        try{
+            response=restTemplate.exchange(uri,
+                    HttpMethod.GET, getHeaders(),String.class);
+        }
+        catch (Exception ex)
+        {
+            return ResponseEntity.ok("not ok");
+        }
+        System.out.println(response.getBody());
         return ResponseEntity.ok(categoryService.findAll());
     }
 
@@ -28,5 +49,10 @@ public class CategoryController {
             return ResponseEntity.ok("null");
     }
 
+    private static HttpEntity<?> getHeaders() throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        return new HttpEntity<>(headers);
+    }
 
 }
