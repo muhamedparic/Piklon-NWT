8a2688b383f4baae0d6153a46fe1751c451d96f7
anjakin <anja261295@gmail.com>
2018-05-29 22:17:29 +0200

    even more stuff

--- piklon-users-frontend/src/app/user-profile/user-profile.component.ts
+++ piklon-users-frontend/src/app/user-profile/user-profile.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-
+  username: string = "Test Username";
+  
   constructor() { }
 
   ngOnInit() {
